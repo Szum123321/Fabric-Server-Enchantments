c3b15d2cfd850aba0e1bfb27e2e7db04da2095a5
Devan-Kerman <devan@cleverpath.com>
2020-05-03 22:49:17 -0500

    remove test code

---
+++ src/main/java/io/github/qspdmc/mixin/RegistrySyncManagerMixin.java
@@ -0,0 +1,24 @@
+package io.github.qspdmc.mixin;
+
+import io.github.qspdmc.ServerEnchantment;
+import net.fabricmc.fabric.impl.registry.sync.RegistrySyncManager;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.MutableRegistry;
+import net.minecraft.util.registry.Registry;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin (value = RegistrySyncManager.class,
+        remap = false)
+public class RegistrySyncManagerMixin {
+	@Redirect (method = "toTag",
+	           at = @At (value = "INVOKE",
+	                     target = "Lnet/minecraft/util/registry/MutableRegistry;getId(Ljava/lang/Object;)Lnet/minecraft/util/Identifier;"))
+	private static <T> Identifier getId(MutableRegistry<T> registry, T entry) {
+		if (registry == Registry.ENCHANTMENT && entry instanceof ServerEnchantment) {
+			return null;
+		}
+		return registry.getId(entry);
+	}
+}
